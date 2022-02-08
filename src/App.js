412a099f9da7cc6504e6fb96f3d47650e7678c6b
artemTolmachev <75881852+artemTolmachev@users.noreply.github.com>
2022-02-08 15:29:34 +0300

    in

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
                 <p>You have everything needed for the extravagant<br/> journey that is your life.</p>
                 <div className='main__social-wr'>
                   <a className='main__social social--fa' href="https://www.facebook.com/profile.php?        id=100061966137095" target='blank'><FaFacebookF size='1em' color='white'/></a>
-                  <a className='main__social social--in' href="https://www.linkedin.com/feed/?trk=guest_homepage-basic_nav-header-signin" target='blank'><FaLinkedinIn size='1em' color='white'/></a>
+                  <a className='main__social social--in' href="https://www.linkedin.com/in/artem-tolmachev-8a5b03203/" target='blank'><FaLinkedinIn size='1em' color='white'/></a>
                 </div>
               </div>
           </div>
